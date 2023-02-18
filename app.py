7caa83b503d5179cd9cb7605a592377a6ff83d62
茂原 <114347526+alexjian2017@users.noreply.github.com>
2023-02-18 16:45:59 +0000

    dynamic pages, store application in database

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, job_detail_from_db, application_to_db
 
 app = Flask(__name__)
 
+company_name = 'Alex'
 @app.route("/")
 def home():
   jobs = load_jobs_from_db()
-  return render_template('home.html',jobs=jobs,company_name='Alex')
+  return render_template('home.html',jobs=jobs,company_name=company_name)
 
 @app.route('/api/jobs')
 def api_job():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route('/job/<id>')
+def job_detail(id):
+  job = job_detail_from_db(id)
+  if not job:
+    return '', 404
+  return render_template('job_detail.html',job=job,company_name=company_name)
+
+@app.route('/job/<id>/apply',methods=['post'])
+def apply_for_job(id):
+  data = request.form
+  if application_to_db(id, data):
+    return render_template('application_submit.html',data=data,company_name=company_name)
+  else:
+    return '',404
+  
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import os
 from sqlalchemy import create_engine, text
+import pymysql
 
 db_connection_str = os.environ['db_connection_str']
 engine = create_engine(
@@ -17,3 +18,24 @@ def load_jobs_from_db():
     for row in result:
       jobs.append(row._asdict())
   return jobs
+
+def job_detail_from_db(id:int):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"select * from jobs where id = {id}"))
+    if not result:
+      return None
+    for row in result:
+      return row._asdict()
+
+def application_to_db(id, data):
+  query = f"insert into applications (job_id,full_name,email,education,work_experience,resume_url) values ({id},'{data['name']}','{data['email']}','{data['education']}','{data['work_experience']}','{data['resume_url']}')"
+  try:
+    with engine.connect() as conn:    
+      conn.execute(text(query))
+    return 1
+  except pymysql.MySQLError as err:
+    print(type(err), err)
+    return 0
+    
+
